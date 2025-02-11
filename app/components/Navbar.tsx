50d41d9842ba8a92a15a9ab1f8e144dad5118fa9
Manupro16 <vera.manuel2004@gmail.com>
2025-02-11 13:44:25 -0600

    Rename and update Navbar component with links and new styles
    Renamed the component to `NavBar` and added dynamic navigation links for better functionality. Implemented new styles in `globals.css` using Tailwind classes to enhance the visual design with a gradient background and shadow effect.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {Flex, Link, Text} from "@radix-ui/themes"
 
-const MyComponent: React.FC = () => {
+const NavBar: React.FC = () => {
+    
+    const navsLinks = [
+        {label: 'HOME', href: '/home'},
+        {label: 'ABOUT', href: '/about'},
+        {label: 'CAREER', href: '/career'},
+        {label: 'STATISTICS', href: '/statistics'},
+        {label: 'ACHIEVEMENTS', href: '/achievements'},
+        {label: 'TEAMS', href: '/teams'},
+        {label: 'PLAYERS', href: '/players'},
+        {label: 'PHILOSOPHY', href: '/philosophy'}
+    ]
+
     return (
-        <div>
-            <h1>Hello, world!</h1>
-        </div>
+        <nav className="nav-footer-background">
+                <Flex as="div" justify="between" align="center" className="px-4 sm:px-6 md:px-10 py-4">
+                    <Link href="/">
+                        <Text as="span" className="text-white" weight="bold" size='4'>Chuy Vera</Text>
+                    </Link>
+                    <Flex as="div" align="center" gap="4">
+                        {navsLinks.map((nav, index) => (
+                            <Link key={index} href={nav.href}  >
+                                <Text as="span" className="text-white hover:text-gray-400 transition-colors duration-500" size='3'>{nav.label}</Text>
+                            </Link>
+                        ))}
+                    </Flex>
+                </Flex>
+        </nav>
     );
 };
 
-export default MyComponent;
+export default NavBar;

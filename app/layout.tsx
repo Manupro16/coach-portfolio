c92aed65831a91dc0e6e15fcbc1209d5dcc122ea
Manupro16 <vera.manuel2004@gmail.com>
2025-02-12 12:36:00 -0600

    Update Theme component to use dark appearance
        Changed the `Theme` component's `hasBackground` prop to `appearance` with a value of "dark" for improved styling. This ensures consistency with the application's dark mode design.

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        <Theme className="website-background" hasBackground={false}>
+        <Theme className="website-background" appearance="dark" >
             <Navbar/>
             {children}
         </Theme>

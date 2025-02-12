0eaedee97dc3127bfea7d932b4e701238a93aed9
Manupro16 <vera.manuel2004@gmail.com>
2025-02-12 14:47:53 -0600

    Enable skeleton loading for ClientNavbar during dynamic import
    Added a `SkeletonNavbar` component to display a placeholder UI while the `Navbar` component is being dynamically imported with SSR disabled. This improves the user experience by providing a visual cue instead of an empty space during loading.

--- app/components/ClientNavbar.tsx
+++ app/components/ClientNavbar.tsx
@@ -2,7 +2,25 @@
 "use client";
 
 import dynamic from 'next/dynamic';
+import React from 'react';
+import {Box, Flex, Link, Text} from "@radix-ui/themes";
 
-const Navbar = dynamic(() => import('./NavBar'), { ssr: false });
+// Dynamically import the actual Navbar with SSR disabled,
+// and provide a fallback that renders a skeleton.
+const Navbar = dynamic(() => import('./NavBar'), {
+  ssr: false,
+  loading: () => <SkeletonNavbar />,
+});
+
+const SkeletonNavbar: React.FC = () => {
+  return (
+    <nav className="nav-footer-background animate-pulse">
+          <Flex as="div" justify="between" align="center" className="px-4 sm:px-6 md:px-10 py-4">
+              <Box className="w-[10%] h-6 bg-gray-600 rounded"></Box>
+              <Box className="w-[15%] h-6 bg-gray-600 rounded"></Box>
+          </Flex>
+    </nav>
+  );
+};
 
 export default Navbar;

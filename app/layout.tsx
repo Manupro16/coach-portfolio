ab36b5ee0a8e63df7e41d9fceccd3dc7815319ce
Manupro16 <vera.manuel2004@gmail.com>
2025-02-12 12:17:09 -0600

    Add gradient background to Theme component
    Introduced a new CSS class `.website-background` to define a gradient background. Applied this class to the Theme component with `hasBackground` set to false to ensure custom styling. This enhances visual consistency across the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        <Theme appearance="dark">
+        <Theme className="website-background" hasBackground={false}>
             <Navbar/>
             {children}
         </Theme>

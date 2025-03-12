7f83bacdd74da85cb1b8d26d12288e67c0a3bb92
lalopoison <lalo.poison2001@gmail.com>
2025-03-12 12:50:46 -0500

    Drop `title` column from `ShowcaseVideo` table.
    This update removes the `title` column from the `ShowcaseVideo` table in both the database schema and Prisma model. Note that all existing data in the `title` column will be lost. Ensure this change aligns with the current data requirements and functionality.

---
+++ prisma/migrations/20250312175021_showcase/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `ShowcaseVideo` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ShowcaseVideo" DROP COLUMN "title";

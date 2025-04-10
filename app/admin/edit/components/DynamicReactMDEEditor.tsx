153bd27c1eb8bcec1327cc5f10ca502056d65791
Manupro16 <vera.manuel2004@gmail.com>
2025-04-10 13:21:30 -0500

    Add @uiw/react-md-editor and related dependencies
    Integrated @uiw/react-md-editor (v4.0.5) into dependencies for markdown editing. Includes additional packages such as @uiw/react-markdown-preview and rehype/remark plugins to enable markdown rendering and syntax highlighting. Updated package-lock.json accordingly.

---
+++ app/admin/edit/components/DynamicReactMDEEditor.tsx
@@ -0,0 +1,30 @@
+// components/DynamicReactMDEditor.tsx
+
+'use client'; // Ensure this component is treated as a client-side component
+
+import React from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import { Box } from '@radix-ui/themes';
+
+interface DynamicReactMDEditorProps {
+    value: string;
+    onChange: (value: string | undefined) => void;
+    colorMode?: 'light' | 'dark';
+}
+
+const DynamicReactMDEditor: React.FC<DynamicReactMDEditorProps> = ({ value, onChange, colorMode }) => {
+    return (
+        <Box className={`rounded-lg shadow-md p-2 transition-colors duration-300 ${colorMode === 'dark' ? 'bg-primaryDark' : 'bg-gray-600'}`}>
+            <MDEditor
+                data-color-mode={colorMode}
+                value={value}
+                onChange={onChange}
+                height={400}
+                preview="live"
+                className="rounded-lg "
+            />
+        </Box>
+    );
+};
+
+export default DynamicReactMDEditor;

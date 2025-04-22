e99bbee992f0e3dab4243c9b27077a8b16c2fd9d
Manupro16 <vera.manuel2004@gmail.com>
2025-04-22 12:00:59 -0500

    Add Zod validation and schema to welcome section API
    Introduced Zod for request validation in the welcome section API, ensuring stricter type and field checks. Updated `package.json` and `package-lock.json` to include the `zod` dependency. Removed `id` handling and refined error messages for better clarity.

--- app/api/homepage/welcome-section/route.ts
+++ app/api/homepage/welcome-section/route.ts
@@ -2,9 +2,17 @@
 
 import {NextResponse} from 'next/server';
 import {prisma} from '@/lib/prisma';
+import { z } from 'zod';
 import {Prisma} from "@prisma/client";
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryUploadResponse  } from '@/lib/cloudinary';
+
+const welcomeSectionSchema = z.object({
+    title: z.string().min(10),
+    subtitle: z.string().min(10),
+    contentTitle: z.string().min(10),
+    contentSubtitle: z.string().min(10),
+}).refine((obj) => Object.keys(obj).length === 0,{ message: 'At least one text field must be present to update' });;
 
 
 export async function GET() {
@@ -27,29 +35,17 @@ export async function PUT(req: Request) {
     try {
         const formData = await req.formData();
         // Extract form fields from the FormData object
-        const id = formData.get('id')?.toString();
         const title = formData.get('title')?.toString();
         const subtitle = formData.get('subtitle')?.toString();
         const imageSrc = formData.get('imageSrc')?.toString();
+        const contentTitle = formData.get('contentTitle')?.toString();
+        const contentSubtitle = formData.get('contentSubtitle')?.toString();
 
-        if (!id) {
-            return NextResponse.json({error: 'ID is required for updating.'}, {status: 400});
-        }
 
-        const updatedRecord = await prisma.welcomeContent.update({
-            where: {id: Number(id)}, // Convert the id to a number
-            data: {
-                title: title ?? undefined,
-                subtitle: subtitle ?? undefined,
-                imageSrc: imageSrc ?? undefined,
-            },
-        });
 
-        return NextResponse.json(updatedRecord, {status: 200});
-    } catch (error: unknown) {
-        const message = error instanceof Error ? error.message : 'Unknown error occurred';
-        return NextResponse.json({error: message}, {status: 500});
-    }
+} catch (error: unknown) {
+    const message = error instanceof Error? error.message : 'Unknown error occurred';
+    return new NextResponse(JSON.stringify({error: message}), {status: 500});}
 }
 
 // npx @prisma/ppg-tunnel --host 127.0.0.1 --port 52604

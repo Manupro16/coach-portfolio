edecdbbb21a960717c1017ce01fa09d15c9a42b7
Manupro16 <vera.manuel2004@gmail.com>
2025-04-22 21:44:35 -0500

    Simplify export of cloudinary instance.
    Added a direct export for the cloudinary instance to streamline imports across the codebase. This change enhances code readability and consistency.

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -2,6 +2,8 @@
 
 import {v2 as cloudinary} from 'cloudinary';
 
+
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -36,3 +38,5 @@ export const uploadImage = async (filePath: string): Promise<CloudinaryUploadRes
     throw error;
   }
 };
+
+export { cloudinary };

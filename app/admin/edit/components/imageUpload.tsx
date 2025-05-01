13cb511d2345c2baa984ec2fcfa4bb3fb5f94444
Manupro16 <vera.manuel2004@gmail.com>
2025-04-30 23:33:18 -0500

    Refactor ImageUpload to improve type safety and usability
    Updated ImageUpload component to use generic types for enhanced type safety with react-hook-form. Replaced the arrow function with a standard function declaration, and refined error handling and file preview logic for better clarity and maintainability.

--- app/admin/edit/components/imageUpload.tsx
+++ app/admin/edit/components/imageUpload.tsx
@@ -1,57 +1,64 @@
 'use client';
 
 import React from 'react';
-import {Box, Heading, Text} from '@radix-ui/themes';
-import {Controller, UseFormReturn} from 'react-hook-form';
+import {Text} from '@radix-ui/themes';
+import {Control, Controller, FieldErrors, FieldValues, UseFormSetValue} from 'react-hook-form';
 
-interface ImageUploadProps {
-    control: UseFormReturn['control'];  // Control object from react-hook-form
-    errors: { [key: string]: { message: string } };
-    setValue: UseFormReturn['setValue'];
+interface ImageUploadProps<TFieldValues extends FieldValues> {
+    control: Control<TFieldValues>;
+    errors: FieldErrors<TFieldValues>;
+    setValue: UseFormSetValue<TFieldValues>;
     colorMode: 'light' | 'dark';
 }
 
-const ImageUpload: React.FC<ImageUploadProps> = ({control, errors, setValue, colorMode}) => {
+
+
+function ImageUpload<TFieldValues extends FieldValues>({
+                                                           control,
+                                                           errors,
+                                                           setValue,
+                                                           colorMode
+                                                       }: ImageUploadProps<TFieldValues>) {
+
+
     return (
-        <Box>
-            <Heading as="h3" size="4" className="font-bold text-primary mb-4">
-                Upload Image
-            </Heading>
-            <label htmlFor="imageFile" className="block text-sm font-medium text-gray-200">
-                Choose an image to upload
-            </label>
-            <Controller
-                control={control}
-                name="image.file"
-                render={({field}) => (
-                    <>
-                        <input
-                            id="imageFile"
-                            type="file"
-                            accept="image/*"
-                            onChange={(e) => {
-                                const files = e.target.files ? Array.from(e.target.files) : [];
-                                field.onChange(files); // Update the form state with the file list
-                                // Generate a preview URL and store it in the form state
-                                if (files.length > 0) {
-                                    const previewUrl = URL.createObjectURL(files[0]);
-                                    setValue('image.previewUrl', previewUrl);
-                                }
-                            }}
-                            className={`mt-1 block w-full ${
-                                colorMode === 'dark' ? 'text-white' : 'text-gray-700'
-                            }`}
-                        />
-                        {errors["image.file"] && (
-                            <Text as="p" id="image-file-error" className="text-red-500 mt-2">
-                                {errors["image.file"].message}
-                            </Text>
-                        )}
-                    </>
-                )}
-            />
-        </Box>
-    );
-};
+
+        <Controller
+            control={control}
+            name="imageFile"
+            render={({field}) => (
+                <>
+                    <input
+                        id="imageFile"
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => {
+                            // Convert FileList to an array
+                            const files = e.target.files ? Array.from(e.target.files) : [];
+                            field.onChange(files); // Pass the array to the form state
+
+                            // Generate a preview URL
+                            if (files && files[0]) {
+                                const previewUrl = URL.createObjectURL(files[0]);
+                                 setValue('imageSrc' as any, previewUrl);
+                            }
+                        }}
+                        className={`mt-1 block w-full ${
+                            colorMode === 'dark' ? 'text-white' : 'text-gray-700'
+                        }`}
+                    />
+                    {errors[field.name as keyof TFieldValues] && (
+                        <Text as="p" className="text-red-500 mt-2">
++                        {errors[field.name as keyof TFieldValues]?.message}
++                      </Text>
+                    )}
+                </>
+            )}
+        />
+    )
+
+}
+
 
 export default ImageUpload;
+

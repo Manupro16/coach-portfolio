e72699be308261396019ff9280ff13eab446b488
Manupro16 <vera.manuel2004@gmail.com>
2025-06-21 23:03:55 -0500

    Refactor VideoCard layout and styles
    - Adjusted flex alignment for improved structure and readability.
    - Replaced unnecessary nested tags with streamlined elements.
    - Added a separator and updated text styling for enhanced aesthetics.
    - Applied consistent spacing and alignment adjustments.

--- app/components/home/components/VideoCard.tsx
+++ app/components/home/components/VideoCard.tsx
@@ -3,52 +3,52 @@
 'use client';
 
 import React from 'react';
-import {Flex, Heading, Text, Card, Box, Badge, Link} from '@radix-ui/themes';
-import { ShowcaseVideo} from '@prisma/client';
+import {Flex, Heading, Text, Card, Box, Badge, Link, Separator} from '@radix-ui/themes';
+import {ShowcaseVideo} from '@prisma/client';
 import {FaPencilAlt} from "react-icons/fa";
 
 type VideoCardProps = ShowcaseVideo & {
-  isAdmin?: boolean;      // optional so you can omit it on public pages
+    isAdmin?: boolean;      // optional so you can omit it on public pages
 };
 
 
-
 const VideoCard: React.FC<VideoCardProps> = ({id, team, season, videoSrc, description, isAdmin,}) => (
-    <Card
-        className="bg-gray-800 bg-opacity-80 backdrop-filter backdrop-blur-md border border-gray-700 rounded-xl shadow-xl overflow-hidden transform transition-transform duration-300">
+    <Card className="website-background">
         <Flex
             direction="column"
-            align="center"
-            justify="center"
-            className="p-6 h-full"
+            align="start"
+            justify="start"
+            className="p-6"
         >
-            <Heading
-                size="4"
-                className="text-gray-100 mb-2 font-sans text-center"
-            >
-                {team}
-            </Heading>
-            {isAdmin && (
-                <Link href={`/admin/edit/home/showcase/${id}`} aria-label="Edit welcome section">
-                    <FaPencilAlt className="w-5 h-5 text-textLight hover:text-primary transition-colors ml-5"/>
-                </Link>
-            )}
-            <Badge className="bg-blue-600 text-white mb-4">{season}</Badge>
-            <Box className="w-full mb-4">
-                   <video
+            <Flex>
+                <Heading size="4" className="text-gray-100 mb-2 font-sans text-center">
+                    {team}
+                </Heading>
+                {isAdmin && (
+                    <Link href={`/admin/edit/home/showcase/${id}`} aria-label="Edit welcome section">
+                        <FaPencilAlt className="w-5 h-5 text-textLight hover:text-primary transition-colors ml-5"/>
+                    </Link>
+                )}
+            </Flex>
+            <Box as="span" className="block h-[3px] w-1/2 bg-primary mt-1"/>
+            <Text as="p" size="4" className="text-textMuted leading-tight mt-2" style={{textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)'}}>
+                {description}
+            </Text>
+            <Separator orientation="horizontal" size="4" color="cyan" className="mt-2"/>
+            <Flex align="center" justify="center" direction="column">
+                <Badge size="3" className="mb-4 mt-4" color="orange">{season}</Badge>
+                <Box className="w-full mb-4">
+                    <video
                         src={videoSrc}
                         controls
                         className="rounded-lg w-full max-w-full"
                         aria-label={`Video highlights of ${team} during the ${season}`}
                     />
-            </Box>
-            <Text
-                size="3"
-                className="text-gray-300 text-center font-sans leading-relaxed"
-            >
-                {description}
-            </Text>
+                </Box>
+            </Flex>
         </Flex>
+
+
     </Card>
 );
 

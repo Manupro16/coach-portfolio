370a806aa62817c70a71b7d7bdd3360fdd580e3f
Manupro16 <vera.manuel2004@gmail.com>
2025-10-10 21:49:09 -0500

    Add `YouthCareer` component to `/about` section and integrate into page layout

---
+++ app/about/components/index.ts
@@ -0,0 +1,10 @@
+// ... existing code ...
+import YouthCareer from './youthCareer'
+import EarlyLife from './earlyLife'
+
+// Re-export them with meaningful, Pascal-cased names
+export { EarlyLife }
+export { YouthCareer }
+
+
+

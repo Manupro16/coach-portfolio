6a1a1b556e496defb20dc7f7261fa6d002b5e8ae
Manupro16 <vera.manuel2004@gmail.com>
2025-10-10 22:26:13 -0500

    Add `ProCoachingCareer` component to `/about` section and integrate into exports

--- app/about/components/index.ts
+++ app/about/components/index.ts
@@ -2,10 +2,11 @@
 import YouthCareer from './youthCareer'
 import EarlyLife from './earlyLife'
 import ProfessionalCareer from './professionalCareer'
+import ProCoachingCareer from './proCoachingCareer'
 
 // Re-export them with meaningful, Pascal-cased names
 export { EarlyLife }
 export { YouthCareer }
 export { ProfessionalCareer }
-
+export { ProCoachingCareer }
 

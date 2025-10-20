b20f280dcb6fa2850073399942b0f6b8d011efb5
Manupro16 <vera.manuel2004@gmail.com>
2025-10-19 22:30:35 -0500

    Add `date` renderer to `WelcomeFormClient` component

--- app/admin/edit/home/welcome/WelcomeFormClient.tsx
+++ app/admin/edit/home/welcome/WelcomeFormClient.tsx
@@ -23,7 +23,8 @@ const COMPONENT_MAP: Record<FieldKind, ShowcaseRenderer> = {
     text: TextFieldRenderer,
     markdown: MarkdownRenderer,
     image: ImageRenderer,
-    video: () => null
+    video: () => null,
+    date: () => null,
 }
 
 
